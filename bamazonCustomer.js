a7e6d269ed5764840dddbaa907d067f57c99a1e5
Aisha <aishaprograms@gmail.com>
2017-01-13 23:54:11 -0600

    package.json made and comments added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,7 +19,7 @@ connection.connect(function(err) {
     if (err) throw err;
     displayProducts();
 });
-
+//Displays all the products in the table using console.table
 function displayProducts() {
     var query = "SELECT item_id, product_name, price FROM products";
     connection.query(query, function(err, res) {
@@ -30,10 +30,12 @@ function displayProducts() {
 }
 
 function buy() {
+    //an array of the two questions
     var questions = [{
         name: 'id',
         message: 'What is the item_id of the product you would like to buy?',
         type: 'input',
+        //validates that the item_id is one of the available item_ids from the table
         validate: function(value) {
             return new Promise(function(resolve, reject) {
                 var validIdQuery = 'SELECT COUNT(item_id) AS count FROM products WHERE item_id = ?';
@@ -42,7 +44,7 @@ function buy() {
                     if (res[0].count === 1) {
                         resolve(true);
                     } else {
-                        resolve('Please enter valid input');
+                        resolve('Please enter the item_id as a number within the range.');
                     }
                 });
             }).then(function(value) {
@@ -53,28 +55,50 @@ function buy() {
         name: 'quantity',
         message: 'How many would you like to purchase?',
         type: 'input',
+        //validates that the input is a number
         validate: function(value) {
-            if (!isNaN(value)) {
+            if (!isNaN(value) && value !== '' && value !== 0) {
                 return true;
             }
-            return 'Please enter a valid input';
+            return 'Please enter a valid quantity as a number.';
         }
     }];
     inquirer.prompt(questions).then(function(ans) {
         var itemQuery = 'SELECT * FROM products WHERE item_id = ?';
         connection.query(itemQuery, [parseInt(ans.id)], function(err, res) {
             if (err) throw err;
+            //if the quantity that was entered is less than or equal to the stock_quantity
             if (parseInt(ans.quantity) <= res[0].stock_quantity) {
                 var newQuantity = res[0].stock_quantity - parseInt(ans.quantity);
+                //updates the item's stock_quantity to be the difference of the purchased quantity
                 var orderQuery = 'UPDATE products SET stock_quantity = ? WHERE item_id = ?';
                 connection.query(orderQuery, [newQuantity, ans.id], function() {
                     if (err) throw err;
                     var totalCost = res[0].price * parseInt(ans.quantity);
+                    console.log('You have chosen to buy ' + ans.quantity + ' of ' + res[0].product_name);
                     console.log('Your total for this order is: $' + totalCost);
+                    nextOrder();
                 });
             } else {
                 console.log('Insufficient quantity! Your order could not be processed.');
+                nextOrder();
             }
         });
     });
+    //function to prompt a new order
+    function nextOrder() {
+        inquirer.prompt({
+                name: 'choice',
+                message: 'Would you like to place another order?',
+                type: 'list',
+                choices: ['Yes', 'No']
+            })
+            .then(function(answers) {
+                if (answers.choice === 'Yes') {
+                    displayProducts();
+                } else {
+                    connection.end();
+                }
+            });
+    }
 }
